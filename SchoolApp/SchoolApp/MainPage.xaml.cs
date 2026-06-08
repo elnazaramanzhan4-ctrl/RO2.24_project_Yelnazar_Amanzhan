ade63e4b2709c8446867775d6783a70a31d5fb32
utezhan1-ops <utezhan1@gmail.com>
2026-06-09 02:32:19 +0500

    MAUI

---
+++ SchoolApp/SchoolApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using SchoolApp.ViewModels;
+
+namespace SchoolApp;
+
+public partial class MainPage : ContentPage
+{
+    private int _count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = new Person();
+    }
+
+    private void OnTapClicked(object sender, EventArgs e)
+    {
+        _count++;
+        StatusLabel.Text = $"Button tapped {_count} time(s).";
+    }
+
+    private async void OnOpenStudentsClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(StudentsPage));
+    }
+
+    private void OnShoutClicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Person p)
+            p.Name = p.Name.ToUpper();
+    }
+}
